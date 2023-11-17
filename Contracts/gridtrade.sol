a22aaacfaa6ed616337ef00b1dcc5a2ad9d01b03
FreeLunchTrading <64173645+0xFLT@users.noreply.github.com>
2023-11-17 10:51:33 -0600

    Update gridtrade.sol

--- Contracts/gridtrade.sol
+++ Contracts/gridtrade.sol
@@ -1,1 +1,71 @@
-f
+// SPDX-License-Identifier:
+
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+
+contract GridTradingContract {
+    AggregatorV3Interface internal priceFeed;
+    IUniswapV2Router02 internal uniswapRouter;
+    address public owner;
+    mapping(address => uint256) public depositTimestamps;
+    mapping(address => uint256) public depositedAmounts;
+    uint256 public constant MINIMUM_USD_DEPOSIT = 1000 * 1e18; // Assuming the USD/ETH price feed has 18 decimals
+    address public constant USDT_ADDRESS = /* USDT Token Address */;
+    uint256 public constant BLOCKS_AFTER_DEPOSIT = 50000;
+
+    // Constructor with Chainlink and Uniswap Router addresses
+    constructor(address _priceFeed, address _uniswapRouter) {
+        owner = msg.sender;
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
+    }
+
+    // Modifier to restrict function access to owner
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
+    }
+
+    // Helper function to get the current ETH/USD price
+    function getETHPrice() public view returns (uint256) {
+        (,int price,,,) = priceFeed.latestRoundData();
+        return uint256(price);
+    }
+
+    // Deposit function with minimum amount check
+    function depositETH() external payable {
+        uint256 ethPrice = getETHPrice();
+        require(msg.value * ethPrice >= MINIMUM_USD_DEPOSIT, "Deposit must be at least $1k worth of ETH.");
+        
+        // Store deposit timestamp and amount
+        depositTimestamps[msg.sender] = block.number;
+        depositedAmounts[msg.sender] = msg.value;
+
+        // Swap half of the ETH for USDT immediately
+        uint256 halfDeposit = msg.value / 2;
+        _swapETHForUSDT(halfDeposit);
+    }
+
+    // Internal function to swap ETH for USDT
+    function _swapETHForUSDT(uint256 _amount) internal {
+        // Add the logic to swap ETH for USDT using Uniswap
+    }
+
+    // Withdraw function with block number check
+    function withdrawETH() external {
+        require(block.number >= depositTimestamps[msg.sender] + BLOCKS_AFTER_DEPOSIT, "You must wait 50k blocks after deposit to withdraw.");
+        require(depositedAmounts[msg.sender] > 0, "No ETH deposited.");
+
+        // Add the logic to swap USDT back to ETH and send the ETH to the depositor
+    }
+
+    // Update grid trading parameters - onlyOwner can call this
+    function updateTradingParameters(/* parameters */) external onlyOwner {
+        // Add the logic to update grid trading parameters
+    }
+
+
+
+}
